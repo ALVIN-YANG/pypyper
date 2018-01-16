0296ebe0287c5b71590ad96a4e148c8b773b3af7
Alvin-Yang <ylq7777777@gmail.com>
2018-01-17 00:00:59 +0800

    feat: init pageing

--- MuxueOnline/apps/organization/views.py
+++ MuxueOnline/apps/organization/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic import View
 
 from .models import CourseOrg, CityDict
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 class OrgView(View):
     """
@@ -15,8 +16,17 @@ class OrgView(View):
         # 城市
         all_citys = CityDict.objects.all()
 
+        # 对课程机构分页
+        p = Paginator(all_orgs, 3, request=request)
+        try:
+            page = int(request.GET.get('page', 1))
+        except PageNotAnInteger:
+            page = 1
+
+        orgs = p.page(page)
+
         return render(request, "org-list.html", {
-            "all_orgs": all_orgs,
+            "all_orgs": orgs,
             "all_citys": all_citys,
             "org_nums": org_nums,
         })
