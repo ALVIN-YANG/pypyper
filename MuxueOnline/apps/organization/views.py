8005690cb95688768b3ffca836b7e2b9e4c7d259
Alvin-Yang <ylq7777777@gmail.com>
2018-01-24 16:32:34 +0800

    feat: sort

--- MuxueOnline/apps/organization/views.py
+++ MuxueOnline/apps/organization/views.py
@@ -5,16 +5,37 @@ from django.views.generic import View
 from .models import CourseOrg, CityDict
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
+
 class OrgView(View):
     """
     课程机构列表功能
     """
+
     def get(self, request):
         # 课程机构
         all_orgs = CourseOrg.objects.all()
         org_nums = all_orgs.count()
+        hot_orgs = all_orgs.order_by("click_num")[:3]
         # 城市
         all_citys = CityDict.objects.all()
+
+        # 取出筛选城市
+        city_id = request.GET.get('city', "")
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 类别筛选
+        category = request.GET.get('ct', "")
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        # 按照学习人数排序
+        sort = request.GET.get('sort', "")
+        if sort:
+            if sort == "students":
+                all_orgs = all_orgs.order_by("-students")
+            elif sort == "courses":
+                all_orgs = all_orgs.order_by("-course_nums")
 
         # 对课程机构分页
         p = Paginator(all_orgs, 3, request=request)
@@ -29,4 +50,8 @@ class OrgView(View):
             "all_orgs": orgs,
             "all_citys": all_citys,
             "org_nums": org_nums,
+            "city_id": city_id,
+            "category": category,
+            "hot_orgs": hot_orgs,
+            "sort": sort,
         })
