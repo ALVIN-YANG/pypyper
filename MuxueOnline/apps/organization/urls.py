85fd76992f14fca39472304c55eb22850b4d3044
Alvin-Yang <ylq7777777@gmail.com>
2018-01-31 18:21:24 +0800

    feat: fav base

--- MuxueOnline/apps/organization/urls.py
+++ MuxueOnline/apps/organization/urls.py
@@ -5,7 +5,7 @@
 # @File    : urls
 
 from django.conf.urls import url, include
-from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView
+from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavView
 
 
 urlpatterns = [
@@ -14,4 +14,8 @@ urlpatterns = [
     url(r'^add_ask/$', AddUserAskView.as_view(), name="add_ask"),
     url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name="org_home"),
     url(r'^course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name="org_course"),
+    url(r'^desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name="org_desc"),
+    url(r'^teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name="org_teacher"),
+    # 机构收藏
+    url(r'^add_fav/$', AddFavView.as_view(), name="add_fav"),
 ]
--- MuxueOnline/apps/organization/views.py
+++ MuxueOnline/apps/organization/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from django.views.generic import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
+import json
 
 from .models import CourseOrg, CityDict
+from operation.models import UserFavorite
 from forms import UserAskForm
 from courses.models import Course
 import json
@@ -102,3 +104,58 @@ class OrgCourseView(View):
             'course_org': course_org,
             'current_page':  current_page,
         })
+
+
+class OrgDescView(View):
+    """
+    机构介绍页
+    """
+    def get(self, request, org_id):
+        current_page = "desc"
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        return render(request, 'org-detail-desc.html', {
+            'course_org': course_org,
+            'current_page':  current_page,
+        })
+
+
+class OrgTeacherView(View):
+    """
+    机构教师页
+    """
+    def get(self, request, org_id):
+        current_page = "teacher"
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        all_teacher = course_org.teacher_set.all()
+        return render(request, 'org-detail-teachers.html', {
+            'all_teacher': all_teacher,
+            'course_org': course_org,
+            'current_page':  current_page,
+        })
+
+
+class AddFavView(View):
+    """
+    用户收藏, 用户取消收藏
+    """
+    def post(self, request):
+        fav_id = request.POST.get('fav_id', 0)
+        fav_type = request.POST.get('fav_type', 0)
+        # 判断用户登录状态
+        if not request.user.is_authenticated():
+            return HttpResponse(json.dumps({'status': 'fail', 'msg': '用户未登录'}), content_type='application/json')
+
+        exist_records = UserFavorite.objects.filter(user=request, fav_id=int(fav_id), fav_type=int(fav_type))
+        if exist_records:
+            # 如果记录已经存在, 表示用户取消收藏
+            exist_records.delete()
+            return HttpResponse(json.dumps({'status': 'success', 'msg': '收藏'}), content_type='application/json')
+        else:
+            user_fav = UserFavorite()
+            if int(fav_id) > 0 and int(fav_type) > 0:
+                user_fav.fav_id = int(fav_id)
+                user_fav.fav_type = int(fav_type)
+                user_fav.save()
+                return HttpResponse(json.dumps({'status': 'success', 'msg': '已收藏'}), content_type='application/json')
+            else:
+                return HttpResponse(json.dumps({'status': 'fail', 'msg': '收藏出错'}), content_type='application/json')
